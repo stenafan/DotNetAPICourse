4eab598796dac0d49bd32f5e794640ce99bd75a5
stenafan <johan.stenhamre@me.com>
2024-04-29 23:15:12 +0200

    WIP 240429 EOB

---
+++ 2-Intermediate/1-HelloWorld_Models/Models/Computers.cs
@@ -0,0 +1,27 @@
+
+using System;
+
+namespace Program.Models;
+
+public class Computer
+{
+  public string Motherboard { get; set; }
+  public int CPUCores { get; set; }
+  public bool HasWifi { get; set; }
+  public bool HasLTE { get; set; }
+  public DateTime ReleaseDate { get; set; }
+  public decimal Price { get; set; }
+  public string VideoCard { get; set; }
+
+  public Computer()
+  {
+    if (VideoCard == null)
+    {
+      VideoCard = "";
+    }
+    if (Motherboard == null)
+    {
+      Motherboard = "";
+    }
+  }
+}
--- 2-Intermediate/1-HelloWorld_Models/Program.cs
+++ 2-Intermediate/1-HelloWorld_Models/Program.cs
@@ -1,60 +1,30 @@
 ﻿using System;
 using System.Data;
 using System.Text.RegularExpressions;
-// using Dapper;
-// using Microsoft.Data.SqlClient;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.EntityFrameworkCore.SqlServer;
-
+using Microsoft.Data.SqlClient;
+using Dapper;
+using Program.Models;
 
 
 namespace HelloWorld
 {
-    public class Computer
-    {       
-        public string Motherboard { get; set; }
-        public int CPUCores { get; set; }
-        public bool HasWifi { get; set; }
-        public bool HasLTE { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public decimal Price { get; set; }
-        public string VideoCard { get; set; }
-
-        public Computer()
-        {
-            if (VideoCard == null)
-            {
-                VideoCard = "";
-            }
-            if (Motherboard == null)
-            {
-                Motherboard = "";
-            }
-        }
-    }
-    internal class Program
+  internal class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+      string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
 
-            IDbConnection dbConnection = new IDbConnection(connectionString);
+      IDbConnection dbConnection = new SqlConnection(connectionString);
 
-            Computer myComputer = new Computer() 
-            {
-                Motherboard = "Z690",
-                HasWifi = true,
-                HasLTE = false,
-                ReleaseDate = DateTime.Now,
-                Price = 943.87m,
-                VideoCard = "RTX 2060"
-            };
-            myComputer.HasWifi = false;
-            Console.WriteLine(myComputer.Motherboard);
-            Console.WriteLine(myComputer.HasWifi);
-            Console.WriteLine(myComputer.ReleaseDate);
-            
-        }
+      string SqlCommand = "SELECT GETDATE()";
+
+      DateTime rightNow = dbConnection.QuerySingle<DateTime>(SqlCommand);
+
+      var test = "God natt";
+
+      Console.WriteLine(rightNow + " Great work buddy!!!!!" +"   " +test);
 
     }
+
+  }
 }
